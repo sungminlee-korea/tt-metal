734f32b8ed2c26f2bd9c80a2841557735804e940
o2buzzle <76864037+o2buzzle@users.noreply.github.com>
2024-09-13 09:57:01 +0000

    #12554: fixes and integration with moreh_dot_backward

--- ttnn/cpp/ttnn/operations/moreh/moreh_dot_op_backward/moreh_dot_backward.hpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_dot_op_backward/moreh_dot_backward.hpp
@@ -6,17 +6,17 @@
 #include "ttnn/decorators.hpp"
 namespace ttnn::operations::moreh::moreh_dot_backward {
 struct MorehDotBackward {
-    static std::vector<Tensor> invoke(
+    static std::vector<std::optional<Tensor>> invoke(
         const Tensor &output_grad,
         const Tensor &input,
         const Tensor &other,
-        std::optional<const Tensor> input_grad,
-        std::optional<const Tensor> other_grad,
+        const std::optional<const Tensor> &input_grad,
+        const std::optional<const Tensor> &other_grad,
         const std::optional<MemoryConfig> &mem_config);
 };
-}
+}  // namespace ttnn::operations::moreh::moreh_dot_backward
 
 namespace ttnn {
-constexpr auto moreh_dot_backward =
-    ttnn::register_operation<"ttnn::moreh_dot_backward", ttnn::operations::moreh::moreh_dot_backward::MorehDotBackward>();
+constexpr auto moreh_dot_backward = ttnn::
+    register_operation<"ttnn::moreh_dot_backward", ttnn::operations::moreh::moreh_dot_backward::MorehDotBackward>();
 }
