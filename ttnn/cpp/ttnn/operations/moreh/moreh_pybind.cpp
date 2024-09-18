b223c454f451027d6390d20c88a20211c6481b7a
Shaw Nguyen <nguyenvanthanhtung2001@gmail.com>
2024-09-18 11:10:18 +0000

    #12805: Refactor moreh_sum_backward

--- ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
@@ -15,19 +15,21 @@
 #include "ttnn/operations/moreh/moreh_nll_loss_backward/moreh_nll_loss_backward_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_nll_loss_unreduced_backward/moreh_nll_loss_unreduced_backward_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_sum/moreh_sum_pybind.hpp"
+#include "ttnn/operations/moreh/moreh_sum_backward/moreh_sum_backward_pybind.hpp"
 
 namespace ttnn::operations::moreh {
 void bind_moreh_operations(py::module &module) {
-    moreh_arange::bind_moreh_arange_operation(module);
     moreh_adam::bind_moreh_adam_operation(module);
-    moreh_getitem::bind_moreh_getitem_operation(module);
-    moreh_sum::bind_moreh_sum_operation(module);
-    moreh_mean::bind_moreh_mean_operation(module);
-    moreh_mean_backward::bind_moreh_mean_backward_operation(module);
+    moreh_arange::bind_moreh_arange_operation(module);
     moreh_dot::bind_moreh_dot_operation(module);
     moreh_dot_backward::bind_moreh_dot_backward_operation(module);
-    moreh_nll_loss_unreduced_backward::bind_moreh_nll_loss_unreduced_backward_operation(module);
-    moreh_nll_loss_backward::bind_moreh_nll_loss_backward_operation(module);
+    moreh_getitem::bind_moreh_getitem_operation(module);
     moreh_matmul::bind_moreh_matmul_operation(module);
+    moreh_mean::bind_moreh_mean_operation(module);
+    moreh_mean_backward::bind_moreh_mean_backward_operation(module);
+    moreh_nll_loss_backward::bind_moreh_nll_loss_backward_operation(module);
+    moreh_nll_loss_unreduced_backward::bind_moreh_nll_loss_unreduced_backward_operation(module);
+    moreh_sum::bind_moreh_sum_operation(module);
+    moreh_sum_backward::bind_moreh_sum_backward_operation(module);
 }
 }  // namespace ttnn::operations::moreh
