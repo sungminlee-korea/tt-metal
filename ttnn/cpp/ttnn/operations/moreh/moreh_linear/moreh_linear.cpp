a6124d4a0dbb1befc942c7fde1ecf25316499a55
DuongQLee <duong.le@moreh.com.vn>
2024-09-18 14:51:00 +0000

    #12694: update  moreh_linear to use new moreh_matmul

--- ttnn/cpp/ttnn/operations/moreh/moreh_linear/moreh_linear.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_linear/moreh_linear.cpp
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "moreh_linear.hpp"
-#include "ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/moreh_matmul/moreh_matmul_op.hpp"
-// #include "ttnn/cpp/ttnn/operations/moreh/moreh_matmul/moreh_matmul.hpp"
+// #include "ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/moreh_matmul/moreh_matmul_op.hpp"
+#include "ttnn/cpp/ttnn/operations/moreh/moreh_matmul/moreh_matmul.hpp"
 namespace ttnn::operations::moreh::moreh_linear {
 std::optional<Tensor> MorehLinear::invoke(
     const Tensor& input,
@@ -14,8 +14,9 @@ std::optional<Tensor> MorehLinear::invoke(
     const std::optional<MemoryConfig>& output_mem_config,
     const DeviceComputeKernelConfig compute_kernel_config) {
     // TODO: LEQUYDUONG change deprecated moreh_matmul to ttnn::moreh_matmul
-    output = tt::operations::primary::moreh_matmul(input, weight, false, true, output, bias, output_mem_config.value_or(input.memory_config()), compute_kernel_config);
-    // output = ttnn::moreh_matmul(input, weight, false, true, output, bias, output_mem_config, compute_kernel_config);
+    // output = tt::operations::primary::moreh_matmul(input, weight, false, true, output, bias,
+    // output_mem_config.value_or(input.memory_config()), compute_kernel_config);
+    output = ttnn::moreh_matmul(input, weight, false, true, output, bias, output_mem_config, compute_kernel_config);
     return output;
 }
 }  // namespace ttnn::operations::moreh::moreh_linear
