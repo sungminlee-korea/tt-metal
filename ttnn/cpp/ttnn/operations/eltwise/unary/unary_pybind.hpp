0b5c7112f8d9f522504c6386b12660e3f339c52b
Bharane AB <bharane.amutha@multicorewareinc.com>
2024-09-25 12:43:01 +0000

    #8143: update sweep test and doc for neg op

--- ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.hpp
@@ -1357,7 +1357,14 @@ void py_module(py::module& module) {
     detail::bind_unary_operation(module, ttnn::log2);
     detail::bind_unary_operation(module, ttnn::logical_not, R"doc(Supports bfloat16 dtype and both TILE and ROW_MAJOR layout)doc");
     detail::bind_unary_operation(module, ttnn::ltz);
-    detail::bind_unary_operation(module, ttnn::neg);
+    detail::bind_unary_operation(module, ttnn::neg,
+    R"doc(Supported dtypes, layouts, and ranks:
+
+        +----------------------------+---------------------------------+-------------------+
+        |     Dtypes                 |         Layouts                 |     Ranks         |
+        +----------------------------+---------------------------------+-------------------+
+        |    BFLOAT16, BFLOAT8_B     |          TILE                   |      2, 3, 4      |
+        +----------------------------+---------------------------------+-------------------+)doc");
     detail::bind_unary_operation(module, ttnn::nez);
     detail::bind_unary_operation_overload_complex_return_complex(module, ttnn::reciprocal);
     detail::bind_unary_operation(module, ttnn::relu);
