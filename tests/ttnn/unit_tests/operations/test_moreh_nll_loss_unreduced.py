7043fe0ab485d19b6894a8a35f2ea99fcd9d2b3b
BuiChiTrung <trungcspntl@gmail.com>
2024-09-25 10:42:37 +0000

    #13095: change testutils path

--- tests/ttnn/unit_tests/operations/test_moreh_nll_loss_unreduced.py
+++ tests/ttnn/unit_tests/operations/test_moreh_nll_loss_unreduced.py
@@ -9,7 +9,7 @@ import pytest
 from models.utility_functions import comp_allclose_and_pcc, is_wormhole_b0
 from loguru import logger
 
-from tests.tt_eager.python_api_testing.unit_testing.misc.test_utils import (
+from tests.ttnn.unit_tests.operations.test_utils import (
     get_compute_kernel_options,
     compute_kernel_options,
     compute_kernel_ids,
