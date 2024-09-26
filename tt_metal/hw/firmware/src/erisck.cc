9286cd2234038e93f6c9ab0f5eefeff046d60656
yugaoTT <yugao@tenstorrent.com>
2024-09-26 16:37:31 +0000

    #0: remove all hardcoded noc_index

--- tt_metal/hw/firmware/src/erisck.cc
+++ tt_metal/hw/firmware/src/erisck.cc
@@ -34,8 +34,8 @@ void __attribute__((section("erisc_l1_code"))) kernel_launch() {
     mailboxes->launch.go.run = RUN_MSG_DONE;
     if (routing_info->routing_enabled and mailboxes->launch.kernel_config.mode == DISPATCH_MODE_DEV) {
         uint64_t dispatch_addr =
-            NOC_XY_ADDR(NOC_X(mailboxes->launch.kernel_config.dispatch_core_x),
-                        NOC_Y(mailboxes->launch.kernel_config.dispatch_core_y), DISPATCH_MESSAGE_ADDR);
+            NOC_XY_ADDR(NOC_X(noc_index, mailboxes->launch.kernel_config.dispatch_core_x),
+                        NOC_Y(noc_index, mailboxes->launch.kernel_config.dispatch_core_y), DISPATCH_MESSAGE_ADDR);
         internal_::notify_dispatch_core_done(dispatch_addr);
     }
 }
--- tt_metal/hw/firmware/src/idle_erisc.cc
+++ tt_metal/hw/firmware/src/idle_erisc.cc
@@ -143,8 +143,8 @@ int main() {
             // Notify dispatcher core that it has completed
             if (mailboxes->launch.kernel_config.mode == DISPATCH_MODE_DEV) {
                 uint64_t dispatch_addr =
-                    NOC_XY_ADDR(NOC_X(mailboxes->launch.kernel_config.dispatch_core_x),
-                        NOC_Y(mailboxes->launch.kernel_config.dispatch_core_y), DISPATCH_MESSAGE_ADDR);
+                    NOC_XY_ADDR(NOC_X(noc_index, mailboxes->launch.kernel_config.dispatch_core_x),
+                        NOC_Y(noc_index, mailboxes->launch.kernel_config.dispatch_core_y), DISPATCH_MESSAGE_ADDR);
                 DEBUG_SANITIZE_NOC_ADDR(noc_index, dispatch_addr, 4);
                 noc_fast_atomic_increment(noc_index, NCRISC_AT_CMD_BUF, dispatch_addr, NOC_UNICAST_WRITE_VC, 1, 31 /*wrap*/, false /*linked*/);
             }
